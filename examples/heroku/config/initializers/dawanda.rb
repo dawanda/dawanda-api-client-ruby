1ccdca5389f2679fd0ef716d5a4f1cbb2db95578
Sascha Depold <sascha@depold.com>
2010-02-19 13:31:20 +0100

    changed api key to dev acc key

--- examples/heroku/config/initializers/dawanda.rb
+++ examples/heroku/config/initializers/dawanda.rb
@@ -1,3 +1,2 @@
-Dawanda.api_key = '01a03715aeafa9227c627bf9f1123879ca644bf1' # de.dawanda.com
-#Dawanda.api_key = 'cdfff20ec201b327434ec3ceb30d186a984bc98b' # de.devanda.com
+Dawanda.api_key = '380d7924396f5596116f3d8815c97dfd8c975582'
 Dawanda.country = 'de'
