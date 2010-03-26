f113a22089b4d29f33ecea77c6a7c3073e58b0f5
Steffen Schröder <steffen@schroeder-blog.de>
2010-03-26 10:34:18 +0100

    refactoring

--- lib/dawanda/oauth.rb
+++ lib/dawanda/oauth.rb
@@ -7,26 +7,16 @@ module Dawanda
   class OAuth
     API_PREFIX = '/api/v1/oauth'
     
-    def self.connect key=nil, secret=nil, params={}
-      @@key = key if key
-      @@secret = secret if secret
-
-      raise "You did not provide a OAuth secret or key" if not @@key or not @@secret
-      
-      @@consumer = ::OAuth::Consumer.new(@@key, @@secret, {
-        :site               => "http://#{Dawanda.country}.#{Dawanda.domain}",
-        :request_token_path => "/oauth/request_token",
-        :access_token_path  => "/oauth/access_token",
-        :authorize_path     => "/oauth/authorize"
-      })
-      
-      if params["oauth_verifier"].present?
-        @@request_token = ::OAuth::RequestToken.from_hash(@@consumer, params) 
-        @@request_token.consumer.options.merge!({:oauth_verifier => params[:oauth_verifier]})
-        @@access_token = @@request_token.get_access_token
-      else
-        @@request_token = @@consumer.get_request_token  
-      end
+    def self.connect key, secret
+      @@consumer = Dawanda::OAuth.create_consumer key, secret
+      @@request_token = @@consumer.get_request_token  
+    end
+    
+    def self.rebuild key, secret, params
+      @@consumer = Dawanda::OAuth.create_consumer key, secret
+      @@request_token = ::OAuth::RequestToken.from_hash(@@consumer, params) 
+      @@request_token.consumer.options.merge!({:oauth_verifier => params[:oauth_verifier]})
+      @@access_token = @@request_token.get_access_token
     end
     
     def self.authorize_url
@@ -47,6 +37,16 @@ module Dawanda
 
     def self.user
       Dawanda::User.new(Dawanda::OAuth.get("#{API_PREFIX}/users.json").result)
+    end
+    
+  private
+    def self.create_consumer key, secret
+      @@consumer = ::OAuth::Consumer.new(key, secret, {
+        :site               => "http://#{Dawanda.country}.#{Dawanda.domain}",
+        :request_token_path => "/oauth/request_token",
+        :access_token_path  => "/oauth/access_token",
+        :authorize_path     => "/oauth/authorize"
+      })
     end
   end
 end
