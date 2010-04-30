2725398f41d706c4313ff26086658a8c81d1b808
Steffen Schröder <steffen@schroeder-blog.de>
2010-04-30 09:38:53 +0200

    handling new provided attributes

--- lib/dawanda/shop.rb
+++ lib/dawanda/shop.rb
@@ -24,7 +24,7 @@ module Dawanda
     attribute :user_id,           [:user, :id]
     attribute :banner_image_url,  [:images, 0, :shop_banner]
 
-    attributes :name, :description
+    attributes :name, :description, :window_title
    
     # Time that this shop was created
     #
